ad29f5e45009d75e213322f50da4c6f53a1f9760
Emer isau <emer.moralesvega@hotmail.com>
2016-08-18 23:38:09 -0500

    Se creo el formulario de medicos

--- controller/cita_controller.js
+++ controller/cita_controller.js
@@ -3,27 +3,44 @@
       var btn_inicio=$(".btn_inicio");
       var btn_usuarios=$(".btn_usuarios");
       var users=$("#users");
-      users.load("includes/usuarios.html");
+      var medico=$("#medico");
        var cita=new Cita();
+        var btn_guardar_paciente;
+      var paciente=new Paciente();
+      medico.load("includes/medico.html");
       btn_inicio.click(function(e){
           e.preventDefault();
           cita.controlar_active(btn_usuarios,btn_inicio);
-          
       });
       btn_usuarios.click(function(e){
          e.preventDefault(); 
           cita.controlar_active(btn_inicio,btn_usuarios);
+          users.load("includes/paciente.html");
+          
+        /**/
       });
+      
   });  
 }());
 class Cita{
     constructor(){
         $(".button-collapse").sideNav();
-
-       
     }
     controlar_active(btn1,btn2){
         btn1.removeClass("active");
         btn2.addClass("active");
+    }
+}
+class Paciente{
+    constructor(){
+    }
+    init(){
+        this.form_pacientes=$("#form_pacientes");
+    }
+    setOnSubmit(){
+        this.form_pacientes.on("submit",function(e){
+           e.preventDefault();
+            alertify.success("siiiii");
+        });
     }
 }
