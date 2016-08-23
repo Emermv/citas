d3c18ae6f4f8b436e8f595903224b1f654e34e0d
Emer isau <emer.moralesvega@hotmail.com>
2016-08-22 22:32:12 -0500

    Creamo las tablas de usuarios, el procedure y el script php

--- php/db.sql
+++ php/db.sql
@@ -1,4 +1,4 @@
-create table pacientes(
+create table usuarios(
 id int not null primary key,
 dni char(8) not null,
 password char(6) not null,
@@ -6,8 +6,43 @@ nombre varchar(50) not null,
 apellidos varchar(50) not null,
  direccion varchar(200) not null,
   telefono varchar(15) not null,
-    email varchar(100),
-    edad int not null,
-    genero char(1) not null,
     ruta_foto varchar(100) not null
-    )
+  );
+  create table pacientes(
+    codigo int not null primary key,
+    id_usuario int not null,
+    edad smallint not null,
+    genero char(1) not null,
+    foreign key (id_usuario) references usuarios(id)
+  )
+  create table medicos(
+    codigo int not null primary key,
+    id_usuario int not null,
+    correo varchar(100) not null,
+    foreign key (id_usuario) references usuarios(id)
+  )
+  create table asistentes(
+    codigo int not null primary key,
+    id_usuario int not null,
+    edad smallint not null,
+    correo varchar(100) not null,
+      genero char(1) not null,
+      foreign key (id_usuario) references usuarios(id)
+  )
+
+  /*procedures*******************************************/
+  DELIMITER //
+  CREATE PROCEDURE sp_login( dni char(8),clave char(6))
+      COMMENT 'sp login'
+      if EXISTS(SELECT u.id from usuarios as u JOIN pacientes as p on u.id=p.id_usuario WHERE u.dni=dni) THEN
+      SELECT  'Paciente' as 'tipo',u.dni,u.password,u.nombre,u.apellidos,u.direccion,u.telefono,u.ruta_foto,p.edad,p.genero
+      from usuarios as u JOIN pacientes as p on u.id=p.id_usuario WHERE u.dni=dni;
+      ELSEIF EXISTS(SELECT u.id from usuarios as u JOIN medicos as m on u.id=m.id_usuario WHERE u.dni=dni) THEN
+         SELECT  'Medico' as 'tipo',u.dni,u.password,u.nombre,u.apellidos,u.direccion,u.telefono,u.ruta_foto,m.correo
+      from usuarios as u JOIN medicos as m on u.id=m.id_usuario WHERE u.dni=dni;
+        ELSEIF EXISTS(SELECT u.id from usuarios as u JOIN asistentes as a on u.id=a.id_usuario WHERE u.dni=dni) THEN
+         SELECT  'Asistente' as 'tipo',u.dni,u.password,u.nombre,u.apellidos,u.direccion,u.telefono,u.ruta_foto,a.edad,a.correo,a.genero
+      from usuarios as u JOIN asistentes as a on u.id=a.id_usuario WHERE u.dni=dni;
+      ELSE
+      SELECT 'Usuario no encontrado';
+      end if;
--- php/login.php
+++ php/login.php
@@ -3,13 +3,39 @@ require_once "server.php";
 $con=conectar();
 if($con){
 mysqli_select_db($con,db);
+/*
 $dni=$_POST['dni'];
 $clave=$_POST['clave'];
-$credenciales=mysqli_query($con,"select p.dni,p.password from pacientes as p where dni='".$dni."'");
+*/
+$dni="87654321";
+$clave="claves";
+
+$credenciales=mysqli_query($con,"call sp_login('".$dni."','".$clave."')");
+$datos =array();
 if($x=mysqli_fetch_array($credenciales)){
   if($dni===$x['dni']){
   if($clave===$x['password']){
-     echo "ok";
+    $datos['code']="1";
+     $datos['dni']=$x['dni'];
+     $datos['nombre']=$x['nombre'];
+     $datos['apellidos']=$x['apellidos'];
+     $datos['direccion']=$x['direccion'];
+     $datos['telefono']=$x['telefono'];
+     $datos['ruta_foto']=$x['ruta_foto'];
+     if($x['tipo']==="Paciente"){
+       $datos['edad']=$x['edad'];
+       $datos['genero']=$x['genero'];
+     }else if($x['tipo']==="Medico"){
+       $datos['correo']=$x['correo'];
+     }else if($x['tipo']==="Asistente"){
+       $datos['edad']=$x['edad'];
+       $datos['correo']=$x['correo'];
+       $datos['genero']=$x['genero'];
+     }else{
+       $datos['code']="-1";
+     }
+     $datos['tipo']=$x['tipo'];
+     echo json_encode($datos);
   }else{
     echo "Clave incorrecto";
   }
